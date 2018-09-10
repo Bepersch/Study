010f3df22febf6589ecdec49cbe9fa657160b361
Sergey <bepersh@gmail.com>
2018-09-10 21:05:43 +0300

    test

---
+++ lesson2HW/arithmetic.py
@@ -0,0 +1,22 @@
+try:
+    n = int(input("Count: "))
+except ValueError:
+    print("NotLikeThis")
+else:
+    arr = []
+    s = 0
+    j = 0
+    while n > 0:
+        try:
+            num = int(input("Number: "))
+        except ValueError:
+            print("NotLikeThis")
+        else:
+            if num > 0:
+                s += 1
+                s += num
+            n -= 1
+    try:
+        print("s: ", s / j)
+    except ZeroDivisionError:
+        print("Zero >0 numbers")
---
+++ lesson2HW/bytes.py
@@ -0,0 +1,12 @@
+try:
+    a = int(input("Number: "))
+    n = str(input("Convert to b or kb: "))
+except ValueError:
+    print("NotLikeThis")
+else:
+    if n == "b":
+        print(a, "kb =", a * 1024, "b")
+    elif n == "kb":
+        print(a, "b =", a / 1024, "kb")
+    else:
+        print("Error Input")
